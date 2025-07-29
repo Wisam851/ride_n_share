40898b08537724dcd6686045d70b7ea3e09bffe6
muhammadhamzaqureshi <hamza@bidecsol.com>
2025-07-29 22:21:06 +0500

    Update ride.module.ts

--- src/ride-socket/ride.module.ts
+++ src/ride-socket/ride.module.ts
@@ -3,9 +3,12 @@ import { RideBookingModule } from 'src/ride-booking/ride-booking.module';
 import { DriverGateway } from './gateways/driver.gateway';
 import { SocketRegisterService } from './socket-registry.service';
 import { CustomerGateway } from './gateways/customer.gateway';
+import { RatingService } from 'src/Rating/rating.service';
+import { RatingModule } from 'src/Rating/rating.module';
 
 @Module({
-  imports: [RideBookingModule],
-  providers: [CustomerGateway, DriverGateway, SocketRegisterService],
+  imports: [RideBookingModule, RatingModule],
+  providers: [CustomerGateway, DriverGateway, SocketRegisterService, RatingService],
+  exports: [CustomerGateway, DriverGateway, SocketRegisterService],
 })
 export class RideModule {}
