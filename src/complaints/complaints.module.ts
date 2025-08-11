1a9cb81a43ec7fd1927551f56e38d8655b741074
FazalDin <fazal@bidecsol.com>
2025-08-12 00:54:20 +0500

    Update complaints.module.ts

--- src/complaints/complaints.module.ts
+++ src/complaints/complaints.module.ts
@@ -4,9 +4,10 @@ import { ComplaintsService } from './complaints.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { complaints } from './entity/complaints.entity';
 import { User } from 'src/users/entity/user.entity';
+import { RideBookingModule } from 'src/ride-booking/ride-booking.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([complaints, User])],
+  imports: [TypeOrmModule.forFeature([complaints, User]), RideBookingModule],
   controllers: [ComplaintsController],
   providers: [ComplaintsService],
 })
